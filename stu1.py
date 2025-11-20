2cdd076820fda320d47e77045f4954ab9f9f6905
Nagaveni.Barker <nagavenibarker6@gmail.com>
2025-11-20 14:36:15 +0530

    Update stu1.py

--- stu1.py
+++ stu1.py
@@ -2,7 +2,7 @@ import sys
 if len(sys.argv)!=3:
   print("Usuage: python stu1.py <name> <rollno>")
   sys.exit(1)
-sprit_name=sys.argv[0]
+scprit_name=sys.argv[0]
 name=sys.argv[1]
 rollno=sys.argv[2]
 print("Script Name:",script_name)
