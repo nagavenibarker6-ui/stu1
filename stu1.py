34a4c8d5ec200df9e08a62f96c9f96738c26efdb
Nagaveni.Barker <nagavenibarker6@gmail.com>
2025-11-20 14:06:39 +0530

    Create stu1.py

---
+++ stu1.py
@@ -0,0 +1,10 @@
+import sys
+if len(sys.argv)!=3:
+  print("Usuage: python stu1.py <name> <rollno>")
+  sys.exit(1)
+sprit_name=sys.argv[0]
+name=sys.argv[1]
+rollno=sys.argv[2]
+print("Script Name:",script_name)
+print("Student Name:",name)
+print("Roll Nmuber:",rollno)
